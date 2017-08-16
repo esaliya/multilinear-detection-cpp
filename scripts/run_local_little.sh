f67aeba1c31c05a8b63f711c287469e432a3ea67
Saliya Ekanayake <esaliya@gmail.com>
2017-08-16 10:41:37 -0400

    Starting rewrite of scan

--- scripts/run_local_little.sh
+++ scripts/run_local_little.sh
@@ -4,7 +4,7 @@ k=3
 delta=1
 alpha=0.15
 epsilon=0.1
-input_file="/Users/esaliya/sali/git/github/esaliya/ccpp/multilinear-detection-cpp/resources/my_little_graph_simple.txt"
+input_file="/Users/esaliya/sali/git/github/esaliya/ccpp/multilinear-detection-cpp-copy/resources/my_little_graph_simple.txt"
 node_count=1
 thread_count=1
 
@@ -12,7 +12,7 @@ thread_count=1
 #mpiexec -np $procs ../cmake-build-debug/main --v $vc --k $k --d $delta --a $alpha --e $epsilon --i $input_file --nc $node_count --tc $thread_count --out out.txt --mms 1  --ibs 4
 
 #alltoallv-worlds
-procs=6
-mpiexec -np $procs ../cmake-build-debug/main --v $vc --k $k --d $delta --a $alpha --e $epsilon --i $input_file --nc $node_count --out out.txt --mms 1  --ibs 2 --pic 2
+procs=1
+mpiexec -np $procs ../cmake-build-debug/main --v $vc --k $k --d $delta --a $alpha --e $epsilon --i $input_file --nc $node_count --out out.txt --mms 20  --ibs 1 --pic 1
 
 
