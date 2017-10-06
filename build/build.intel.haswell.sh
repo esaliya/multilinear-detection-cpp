24baad5d320e6adac7f71c14821d3383a046f144
Saliya Ekanayake <esaliya@hswlogin1.cm.cluster>
2017-10-06 09:47:13 -0400

    modifying build haswell script and cmake list

--- build/build.intel.haswell.sh
+++ build/build.intel.haswell.sh
@@ -2,6 +2,6 @@
 
 #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/cm/shared/apps/mpich/ge/gcc/64/3.1/bin/mpicxx -DCMAKE_C_COMPILER=/cm/shared/apps/mpich/ge/gcc/64/3.1/bin/mpicc -DBOOST_ROOT=/home/esaliya/sali/software/boost_1_63_0 -DBOOST_INCLUDEDIR=/home/esaliya/sali/software/builds/boost/include -DBOOST_LIBRARYDIR=/home/esaliya/sali/software/builds/boost/lib ..
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/home/esaliya/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/bin/mpicxx -DCMAKE_C_COMPILER=/home/esaliya/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/bin/mpicc -DBOOST_ROOT=/home/esaliya/sali/software/boost_1_64_0 -DBOOST_INCLUDEDIR=/home/esaliya/sali/software/builds/boost/include -DBOOST_LIBRARYDIR=/home/esaliya/sali/software/builds/boost/lib ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/home/esaliya/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/bin/mpicxx -DCMAKE_C_COMPILER=/home/esaliya/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/bin/mpicc -DBOOST_ROOT=/home/esaliya/sali/software/boost_1_64_0 -DBOOST_INCLUDEDIR=/home/esaliya/sali/software/builds/boost/include -DBOOST_LIBRARYDIR=/home/esaliya/sali/software/builds/boost/lib ..
 
 make
