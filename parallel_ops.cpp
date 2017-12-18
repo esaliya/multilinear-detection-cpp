cfa235565e5dccc7822ec7834bfdbf7f25349ffd
Saliya Ekanayake <esaliya@gmail.com>
2017-12-18 11:28:08 -0500

    micro timings

--- parallel_ops.cpp
+++ parallel_ops.cpp
@@ -258,7 +258,7 @@ void parallel_ops::find_nbrs(int global_vertex_count, int local_vertex_count, st
   }
 #endif
 
-  /* The following sections are very subtle, so keep your eyes open */
+  /* Reminder to self: The following sections are very subtle, so keep your eyes open */
 
   // BEGIN ----------------
   start_ms = std::chrono::high_resolution_clock::now();
@@ -317,7 +317,7 @@ void parallel_ops::find_nbrs(int global_vertex_count, int local_vertex_count, st
     delete inverse_map;
   }
 
-//#ifdef LONG_DEBUG
+#ifdef LONG_DEBUG
   // perf counter
   if (instance_id == 0 && instance_proc_rank == 0){
     std::cout<<"** Instance 0 Rank 0 writing perf output to "<< out_file;
@@ -354,7 +354,7 @@ void parallel_ops::find_nbrs(int global_vertex_count, int local_vertex_count, st
     }
   }
   out_fs.close();
-//#endif
+#endif
 
   delete [] all_msg_counts;
   all_msg_counts = nullptr;
